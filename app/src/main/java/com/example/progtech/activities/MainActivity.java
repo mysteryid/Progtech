7d841bf89b658b9e51ce5f411175293d88ffb745
Reiner Anggriawan Jasin <reinerjasin@gmail.com>
2020-04-27 17:15:07 +0800

    - Membuat package activitie, adapter, fragment, model - Membuat fragment home, subject, dan tips - Membuat resource 'menu' - Memperbaiki color primary, primary_dark, accent - Membuat menu item di bottom_nav_menu.xml

--- app/src/main/java/com/example/progtech/activities/MainActivity.java
+++ app/src/main/java/com/example/progtech/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.progtech;
+package com.example.progtech.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.progtech.R;
 
 public class MainActivity extends AppCompatActivity {
 
