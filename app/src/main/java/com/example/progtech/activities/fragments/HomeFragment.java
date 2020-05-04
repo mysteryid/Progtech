63803e834058f91d7dedacdaf828ef18cc5fcb31
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2020-05-04 20:33:00 +0800

    Hubungkan database dengan recycler view done

--- app/src/main/java/com/example/progtech/activities/fragments/HomeFragment.java
+++ app/src/main/java/com/example/progtech/activities/fragments/HomeFragment.java
@@ -4,20 +4,31 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.progtech.R;
+import com.example.progtech.activities.adapter.CardStudent;
+import com.example.progtech.activities.model.Student;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link HomeFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
@@ -34,32 +45,7 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView rvStudent;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -80,12 +66,44 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button btn_home = view.findViewById(R.id.btn_home);
-        btn_home.setOnClickListener(new View.OnClickListener() {
+        rvStudent = view.findViewById(R.id.rv_student);
+        getStudent();
+    }
+
+    private void getStudent() {
+        final ArrayList<Student> students = new ArrayList<>();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://reinerjasin.000webhostapp.com/api/list.php";
+
+        client.get(url, new AsyncHttpResponseHandler() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("student");
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject obj = list.getJSONObject(i);
+                        Student s = new Student(obj.getString("id"), obj.getString("nim"), obj.getString("name"), obj.getString("email"), obj.getString("phone"), obj.getString("gender"));
+                        students.add(s);
+                    }
+                    showStudent(students);
+                } catch (Exception e) {
+                    Log.d("ExceptionStudent", "onSuccess: " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("OnFailureStudent", "onFailure: " + error.getMessage());
             }
         });
+    }
+
+    private void showStudent(ArrayList<Student> students) {
+        rvStudent.setLayoutManager(new LinearLayoutManager(getActivity()));
+        CardStudent cardStudent = new CardStudent(getContext());
+        cardStudent.setListStudent(students);
+        rvStudent.setAdapter(cardStudent);
     }
 }
