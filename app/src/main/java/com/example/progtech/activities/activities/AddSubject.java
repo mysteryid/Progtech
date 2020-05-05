08d2aad32d155622da7646d9c9069df9d87bf825
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2020-05-05 16:31:42 +0800

    revisi intent di AddSubject.java

--- app/src/main/java/com/example/progtech/activities/activities/AddSubject.java
+++ app/src/main/java/com/example/progtech/activities/activities/AddSubject.java
@@ -52,7 +52,7 @@ public class AddSubject extends AppCompatActivity {
         btnViewData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                Intent intent = new Intent(AddSubject.this, ListDataActivity.class);
                 startActivity(intent);
             }
         });
