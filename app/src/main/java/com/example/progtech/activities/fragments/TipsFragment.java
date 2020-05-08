6cbce6e5896a4d9d8980b1c4ca17f4c46c2179d0
mysteryid <62708654+mysteryid@users.noreply.github.com>
2020-05-08 22:23:04 +0700

    balik yg ke sebelumnya yg blm eror

--- app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
+++ app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,19 +15,13 @@ import androidx.fragment.app.Fragment;
 import com.example.progtech.R;
 
 public class TipsFragment extends Fragment {
+    private Bundle savedInstanceState;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tips, container, false);
     }
 
-    public void browser1(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://lifehacker.com/how-to-use-mind-maps-to-unleash-your-brains-creativity-1348869811"));
-        startActivity(browserIntent);
-    }
 
-    public void browser2(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.focusboosterapp.com/blog/make-the-pomodoro-technique-work-for-you-five-tips-to-personalise-your-pomodoros/"));
-        startActivity(browserIntent);
-    }
 }
