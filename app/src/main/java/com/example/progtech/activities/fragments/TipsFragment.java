f35ce5c312f00cf250dab6aa5d3017a65b0a7ef4
mysteryid <62708654+mysteryid@users.noreply.github.com>
2020-05-08 12:22:20 +0700

    tips...tapi kalo click link force close

--- app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
+++ app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
@@ -1,5 +1,7 @@
 package com.example.progtech.activities.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,5 +18,15 @@ public class TipsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tips, container, false);
+    }
+
+    public void browser1(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://lifehacker.com/how-to-use-mind-maps-to-unleash-your-brains-creativity-1348869811"));
+        startActivity(browserIntent);
+    }
+
+    public void browser2(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.focusboosterapp.com/blog/make-the-pomodoro-technique-work-for-you-five-tips-to-personalise-your-pomodoros/"));
+        startActivity(browserIntent);
     }
 }
