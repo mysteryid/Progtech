f1775a8a3eabf0e7020982a838752b16d44af008
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2020-05-08 23:46:15 +0800

    Please work!

--- app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
+++ app/src/main/java/com/example/progtech/activities/fragments/TipsFragment.java
@@ -34,14 +34,4 @@ public class TipsFragment extends Fragment {
         });
                 return view;
     }
-
-    public void browser1(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://lifehacker.com/how-to-use-mind-maps-to-unleash-your-brains-creativity-1348869811"));
-        startActivity(browserIntent);
-    }
-
-    public void browser2(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.focusboosterapp.com/blog/make-the-pomodoro-technique-work-for-you-five-tips-to-personalise-your-pomodoros/"));
-        startActivity(browserIntent);
-    }
 }
